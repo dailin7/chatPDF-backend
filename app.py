9b3a77b6b65ad62475fa89b0f13af946b121d7d3
Ziheng Cai <70208314+LeonCai1@users.noreply.github.com>
2023-05-08 17:25:19 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import streamlit as st
-from streamlit.components.v1 import html
-from streamlit_chat import message
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.document_loaders import TextLoader
@@ -12,14 +9,6 @@ from langchain.chains import RetrievalQA
 import os
 from dotenv import load_dotenv
 
-def init_page():
-    st.set_page_config(page_title='chatPDF')
-    st.title('chatPDF')
-    with st.sidebar:
-        uploaded_files = st.file_uploader('Choose your PDF files', type=['pdf', 'PDF'], accept_multiple_files=True)
-init_page()
-
-@st.cache_data
 def load_env():
     load_dotenv()
     OPEN_AI_KEY = os.getenv("OPEN_AI_KEY")
@@ -28,7 +17,7 @@ def load_env():
 #TODO
 # take a deeper look at this part
 # how to process a pdf file(embedding model)
-@st.cache_resource
+
 def load_chain():
     #load and split input files
     loader = TextLoader("../chatPDF/test_date/sample.txt")
@@ -46,33 +35,10 @@ def load_chain():
 
     return qa
 
-def init_conversation():
-    if 'hist_questions' not in st.session_state:
-        st.session_state['hist_questions'] = []
-
-    if 'hist_responses' not in st.session_state:
-        st.session_state['hist_responses'] = []
-
-#TODO should the answer also be added into the DB or the llm for better context and thus better performance in long term?
-def ask_and_answer():
-    question = st.text_input(label = 'Type in your question', value=None)
-
-    if question:
-        ans = qa.run(question)
-        st.session_state['hist_questions'].append(question)
-        st.session_state['hist_responses'].append(ans)
-
-def show_conversation():
-    if st.session_state['hist_responses']:
-        for i in range(len(st.session_state['hist_responses']) - 1):
-            message(st.session_state['hist_responses'][i])
-            message(st.session_state['hist_questions'][i], is_user=True)
 
 env = load_env()
-init_conversation()
 qa = load_chain()
-ask_and_answer()
-show_conversation()
+print(qa.run('what is babyAGI'))
 
 
 
