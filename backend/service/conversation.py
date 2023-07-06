1feee5c626b7ac76377a5eb6632dd9740acc142e
Ziheng Cai <70208314+LeonCai1@users.noreply.github.com>
2023-07-06 10:46:59 +0800

    change qa history format

--- backend/service/conversation.py
+++ backend/service/conversation.py
@@ -16,8 +16,12 @@ def create_conversation(data):
 def update_conversation_history(conversation_name, question, answer):
     conversation = Conversation.objects.get(conversation_name=conversation_name)
     history = conversation.conversation_history
-    conversation.conversation_history.append(
-        {"question": answer["question"], "answer": answer["answer"]}
+    conversation.conversation_history += format_q_a(
+        answer["question"], answer["answer"]
     )
     conversation.save()
     return conversation.conversation_history
+
+
+def format_q_a(question: str, answer: str):
+    return [{"content": question, "direction": 1}, {"content": answer, "direction": 0}]
